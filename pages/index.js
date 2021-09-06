f7baee2b57434bd952b83d9bc81c322f7a576212
Larry <olalerulanre@gmail.com>
2021-09-06 20:06:34 +0100

    Home styles updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 import Body from "../components/Body";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
