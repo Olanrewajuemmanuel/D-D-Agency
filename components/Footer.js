6c31dd6c275aba78a1a615ba7812a174547de2ff
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2022-08-15 17:45:15 +0100

    Footer new file

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,15 @@
 export default function Footer() {
-  return (
-    <footer>
-      <div>
-        <ul className={''}>
-          <li>.Twitter</li>
-          <li>.dribbble</li>
-          <li>.Instagram</li>
-          <li>.LinkedIn</li>
-        </ul>
-      </div>
-    </footer>
-  );
-}
+    return (
+      <footer>
+        <div>
+          <ul className={''}>
+            <li>.Twitter</li>
+            <li>.dribbble</li>
+            <li>.Instagram</li>
+            <li>.LinkedIn</li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+  
